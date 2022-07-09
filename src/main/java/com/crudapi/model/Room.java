a778b4977e014c69e0b138dfb0c87d9eeaf59ac2
Mikael <mikaelmuniz2001@gmail.com>
2022-07-09 15:20:00 -0300

    adding a model to my 'Rooms'

--- src/main/java/com/crudapi/model/Room.java
+++ src/main/java/com/crudapi/model/Room.java
@@ -1,4 +1,24 @@
 package com.crudapi.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private int members;
+
+    @Column(name = "nome", length = 60)
+    private String name;
+
+    @Column
+    private String owner;
+
 }
